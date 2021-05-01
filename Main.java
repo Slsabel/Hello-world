1b020d62bfc02b50287755065fca999535a80a2b
Slsabel <83460929+Slsabel@users.noreply.github.com>
2021-05-01 02:47:38 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    private static System system;
+
+    public static void main(String[] args) {
+
+        system.out.println(" Hello world ");
+    }
+}
